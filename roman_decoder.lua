618da466ba1cb4216e1ed6bd7315b536809ab734
jedeker <56451734+jedeker@users.noreply.github.com>
2021-04-29 14:32:03 -0500

    Create roman_decoder.lua

---
+++ roman_decoder.lua
@@ -0,0 +1,33 @@
+local solution = {}
+
+-- Create a function that takes a Roman numeral as its argument and returns
+-- its value as a numeric decimal integer.
+local function chars(str)
+  local function iter(str, i)
+    i = i + 1
+    if i <= str:len() then
+      return i, str:sub(i, i)
+    end
+  end
+  return iter, str, 0
+end
+
+kata.roman_Decoder = function(roman) 
+  local symbols = {["I"] = 1, ["V"] = 5, ["X"] = 10, ["L"] = 50, ["C"] = 100, ["D"] = 500, ["M"] = 1000}
+  local value, previous = 0, nil;
+  for i, c in chars(roman:reverse()) do
+    if previous then
+      if symbols[c] < previous then
+        value = value - symbols[c]  
+      else
+        value = value + symbols[c]
+      end
+    else
+      value = value + symbols[c]
+    end
+    previous = symbols[c]
+  end
+  return value
+end
+
+return kata
